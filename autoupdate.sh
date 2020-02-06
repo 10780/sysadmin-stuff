5adb178f7be3374fdc248599c4c054ae8edbc6f8
10780 <reezius@protonmail.com>
2020-02-06 13:26:40 -0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This is like ezupdate.sh file, only meant to be run specifically as cron job instead
+printf "Beginning automatic update\n"
 printf "Updating...\n"
 apt-get update && apt-get dist-upgrade -y
 apt-get autoclean
