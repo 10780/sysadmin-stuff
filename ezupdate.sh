77a0504e785b848db246c91012e8d352b924bdb7
10780 <reezius@protonmail.com>
2020-02-06 13:15:26 -0800

    Update ezupdate.sh

--- ezupdate.sh
+++ ezupdate.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 printf "Updating...\n"
-apt-get update
-apt-get upgrade -y
-apt-get autoclean
+apt-get update && apt-get upgrade
 
 printf "Update complete\n"
 date '+%a %d %B %T %s'
-
-#NOTE- this can be run manually (using sudo) but works best as cron job
