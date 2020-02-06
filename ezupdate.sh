46b3fc79056533745a97714aa5fbc42209ba8f55
10780 <reezius@protonmail.com>
2020-02-06 13:19:17 -0800

    Update ezupdate.sh

--- ezupdate.sh
+++ ezupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#like autoupdate.sh except this was usually run manually
 printf "It's update time!"
-apt-get update && apt-get upgrade
+apt-get update && apt-get dist-upgrade
 
 printf "\nUpdate complete\n"
 date '+%a %d %B %T %s'
